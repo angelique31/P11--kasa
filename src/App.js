e81d2a9faef69aba709582e6545c9f81b56932d4
angelique_rosin <angie1979.capucine@yahoo.fr>
2023-02-26 21:42:41 +0100

    1ere version

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About/About";
+import NotFoundPage from "./pages/Error/NotFoundPage";
+import Home from "./pages/Home/Home";
+import LocationPage from "./pages/LocationPage/LocationPage";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/locationPage" element={<LocationPage />} />
+        <Route path="/notFoundPage" element={<NotFoundPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
